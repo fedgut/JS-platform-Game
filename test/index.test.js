562934459340cb10d41d98081b93d315ed86ec1a
fedgut <fedgut@gmail.com>
2020-05-11 16:50:54 -0500

    create dom elements to ask for user name

--- test/index.test.js
+++ test/index.test.js
@@ -29,5 +29,5 @@ export default class Game extends Phaser.Game {
 window.game = new Game();
 
 test('should be a testament of the proper configuration of jest to handle ES6, import and canvas, working with webpacked assets (with mocking for images and css)', () => {
-  expect(true).toBe(true);
+  expect(window.game).not.toBe(undefined);
 });
